25fdb9f96c580c728a4472d1e67061c53157c64c
Rohith <rohithsekharnaduvath@gmail.com>
2023-07-21 20:44:26 +0530

    Simplified the constructor

--- lib/quizbrain.dart
+++ lib/quizbrain.dart
@@ -4,20 +4,16 @@ class QuizBrain {
   int _questionNumber = 0;
 
   final List<Question> _questionBank = [
-    Question(q: 'The Great Wall of China can be seen from space.', a: false),
-    Question(q: 'The Earth\'s moon is larger than Pluto.', a: false),
-    Question(q: 'The human body has four lungs.', a: false),
-    Question(q: 'Brazil is the largest country in South America.', a: true),
-    Question(
-        q: 'The Statue of Liberty was a gift from France to the United States.',
-        a: true),
-    Question(q: 'Some cats are actually allergic to humans', a: true),
-    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
-    Question(
-        q: 'Approximately one quarter of human bones are in the feet.',
-        a: true),
-    Question(q: 'A slug\'s blood is green.', a: true),
-    Question(q: 'Buzz Aldrin\'s mother\'s maiden name was \"Moon\".', a: true),
+    Question('The Great Wall of China can be seen from space.',false),
+    Question('The Earth\'s moon is larger than Pluto.',false),
+    Question('The human body has four lungs.',false),
+    Question('Brazil is the largest country in South America.',true),
+    Question('The Statue of Liberty was a gift from France to the United States.',true),
+    Question('Some cats are actually allergic to humans',true),
+    Question('You can lead a cow down stairs but not up stairs.',false),
+    Question('Approximately one quarter of human bones are in the feet.',true),
+    Question('A slug\'s blood is green.',true),
+    Question('Buzz Aldrin\'s mother\'s maiden name was  \"Moon\".',true),
   ];
 
   void nextQuestion() {
